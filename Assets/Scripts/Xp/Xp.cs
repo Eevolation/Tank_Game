43b8929aa154f0890e484fd0ddcbfa34d3b6947a
Bowyn257 <s192254@students.aie.edu.au>
2020-02-26 20:11:49 +1030

    added new box collisions to some of the scene, and refining the xp and level system more with added buffs

--- Assets/Scripts/Xp/Xp.cs
+++ Assets/Scripts/Xp/Xp.cs
@@ -6,6 +6,9 @@ public class Xp : MonoBehaviour
 {
     public int XP;
     public int Currentlevel;
+    public float damage = PlayerShell.damage;
+    private float maxhealth = PlayerTankHealth.m_StartingHealth;
+    private float currenthealth = PlayerTankHealth.m_CurrentHealth;
 
     // Update is called once per frame
     void Update()
@@ -16,14 +19,25 @@ public class Xp : MonoBehaviour
     public void UpdateXp(int xp)
     {
         XP += xp;
-        float damage = PlayerShell.damage;
 
         int curlvl = (int)(0.1f * Mathf.Sqrt(XP));
 
         if(curlvl != Currentlevel)
         {
             Currentlevel = curlvl;
-            PlayerShell.damage += 10;
+            if(Currentlevel == 2)
+            {
+                PlayerShell.damage += 5;
+            }
+            if(Currentlevel == 3)
+            {
+                PlayerTankHealth.m_StartingHealth += 5;
+                PlayerTankHealth.m_CurrentHealth = PlayerTankHealth.m_StartingHealth;
+            }
+            if (Currentlevel == 4) ;
+            {
+                PlayerShell.damage += 5;
+            }
             // add some cool text show you reached a new level
         }
 
